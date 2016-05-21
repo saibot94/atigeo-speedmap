d98e3745b55f2f294064b5e70b854ccc1267abfd
Emanuel Danci <email>
2016-05-21 23:51:25 +0300

    Packaging

--- api/domain/mongo.py
+++ api/domain/mongo.py
@@ -13,3 +13,13 @@ def get_points(count=None):
     points_cursor = db.drives.find({"speed": {"$gte": DISPLAY_SPEED}},
                                    {'latitude': 1, 'longitude': 1, 'speedkmh': 1, '_id': 0})
     return points_cursor if not count else points_cursor.limit(count)
+
+
+def new_point(point):
+    return {'latitude': point['latitude'], 'longitude': point['longitude'], 'weight': point['speedkmh'] / 10}
+
+
+def get_points_with_weight(count=None):
+    points = get_points(count=count)
+    points_with_weight = [new_point(point) for point in points]
+    return points_with_weight
