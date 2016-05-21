c2ab67dbca65f830e3dbda26408a43e50a0c4bb1
Ciprian Lucaci <ciprian.lucaci@atigeo.com>
2016-05-21 20:30:43 +0300

    switch to mongodb

--- hacktm16-elasticsearch-insert/src/main/java/com/atigeo/hacktm16/handler/App.java
+++ hacktm16-elasticsearch-insert/src/main/java/com/atigeo/hacktm16/handler/App.java
@@ -1,16 +1,20 @@
 package com.atigeo.hacktm16.handler;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
 import org.apache.log4j.PropertyConfigurator;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Created by cipri on 5/21/16.
@@ -19,7 +23,10 @@ public class App {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-    public static void main(String[] args){
+    private MongoClient mongoClient;
+    private MongoDatabase mongoDatabase;
+
+    public static void main(String[] args) throws InterruptedException {
         PropertyConfigurator.configure(App.class.getClassLoader().getResourceAsStream("log4j.xml"));
 
 //        String ip = args[0];
@@ -30,12 +37,31 @@ public class App {
         }
         String dumpDirectory = args[0];
 
+        App app = new App();
+
+        try {
+            app.connectMongoDb();
+        } catch (UnknownHostException e) {
+           LOGGER.error("mongo connection", e);
+            return;
+        }
+
         LOGGER.info(String.format("Started with dir: %s", dumpDirectory));
-        crawlDirectory(dumpDirectory);
+        while(true) {
+            LOGGER.info("crawling ... ");
+            app.crawlDirectory(dumpDirectory);
+            LOGGER.info("sleeping ... ");
+            Thread.sleep(10 * 1000);
+        }
     }
 
+    private  void connectMongoDb() throws UnknownHostException {
+        mongoClient = establishESConnection();
 
-    private static void crawlDirectory(String dumpDirectoryPath){
+        mongoDatabase = mongoClient.getDatabase("hacktm");
+    }
+
+    private void crawlDirectory(String dumpDirectoryPath){
 
         File dumpDirectory = new File(dumpDirectoryPath);
         if(!dumpDirectory.isDirectory()){
@@ -46,41 +72,53 @@ public class App {
         File[] listOfFiles = dumpDirectory.listFiles();
         Arrays.sort(listOfFiles);
 
-        for(File file: listOfFiles){
-            LOGGER.info(String.format("dumping file: %s", file.getAbsolutePath()));
+        for(int i=0; i<listOfFiles.length - 1; i++){
+            File file = listOfFiles[i];
 
-            archiveProcessedFile(file);
+            if(file.isDirectory())
+                continue;
+
+            String fileName = file.getName();
+            if(!fileName.startsWith("drive_dump"))
+                continue;
+
+            LOGGER.info(String.format("dumping file: %s", file.getName()));
+
+            String parentPath = file.getParent();
+            String archivedFile = parentPath +File.separator+ "archived" +File.separator + file.getName();
+            LOGGER.info(String.format("archived to: %s", archivedFile));
+
+            processFile(file.getAbsolutePath(), archivedFile);
+
+            file.delete();
         }
     }
 
-    private static void archiveProcessedFile(File oldFile){
-        try{
-            String parentPath = oldFile.getParent();
-            LOGGER.info(String.format("Moving file: %s from dir: %s", oldFile.getName(), parentPath));
 
-            if(oldFile.renameTo(new File(parentPath +File.pathSeparator+ "old" +File.pathSeparator + oldFile.getName()))){
-                LOGGER.info("File is moved successful!");
-            }else{
-                LOGGER.warn("File is failed to move!");
-            }
+    private void processFile(String fileName, String archivedFile){
+        LOGGER.info(String.format("PROCESSING FILE %s", fileName));
 
-        }catch(Exception e){
-            LOGGER.error("file archive error", e);
-        }
-    }
+        MongoCollection<Document> collection = mongoDatabase.getCollection("people");
 
-    private static void readFile(String fileName){
 
         try {
             FileReader fileReader = new FileReader(fileName);
+            FileWriter fileWriter = new FileWriter(archivedFile);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
             String line ;
+            List<Document> documents = new ArrayList<>();
             while((line = bufferedReader.readLine()) != null) {
-                processLine(line);
+                Document dbObject = processLine(line);
+                documents.add(dbObject);
+                bufferedWriter.write(line);
             }
 
-            // Always close files.
+            collection.insertMany(documents);
+
             bufferedReader.close();
+            bufferedWriter.close();
         }
         catch(FileNotFoundException ex) {
             LOGGER.error(String.format("Unable to open file %s", fileName), ex);
@@ -91,26 +129,70 @@ public class App {
 
     }
 
-    private static void processLine(String line){
+    private Document processLine(String line){
         LOGGER.info(String.format("line read: %s", line));
+        Document dbObject = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode jsonNode = mapper.readValue(line, ObjectNode.class);
+
+            jsonNode = processJsonNode(jsonNode);
+            LOGGER.info(String.format("processed : %s", jsonNode.asText()));
+
+            dbObject = (Document) JSON.parse(jsonNode.toString());
 
 
+        } catch (IOException e) {
+            LOGGER.error("json exception");
+        } catch (Exception e) {
+            LOGGER.error("json exception");
+        }
+        return dbObject;
+    }
+
+    private ObjectNode processJsonNode(ObjectNode jsonNode) throws Exception{
+
+        //process speed
+        JsonNode speedValue = jsonNode.get("speed");
+        double speedDoubleMs = speedValue.asDouble();
+        double speedDoubleKmh = 3.6 * speedDoubleMs;
+
+        jsonNode = jsonNode.put("speedkmh", speedDoubleKmh);
+        jsonNode = jsonNode.put("speedms",speedDoubleMs);
+
+        //unix time
+        JsonNode driveTimeValue = jsonNode.get("drivetime");
+        String driveTime = driveTimeValue.asText();
+        String year = driveTime.substring(0, 4);
+        String month = driveTime.substring(4, 6);
+        String day = driveTime.substring(6, 8);
+        String hour = driveTime.substring(8, 10);
+        String minute = driveTime.substring(10, 12);
+        String second = driveTime.substring(12, 14);
+        String ms = driveTime.substring(14, 15);
+
+        int y = Integer.valueOf(year);
+        int m = Integer.valueOf(month);
+        int d = Integer.valueOf(day);
+        int hh = Integer.valueOf(hour);
+        int mm = Integer.valueOf(minute);
+        int ss = Integer.valueOf(second);
+        int mss = Integer.valueOf(ms);
+
+        Calendar calendar = new GregorianCalendar(y, m, d, hh, mm, ss);
+        calendar.set(Calendar.MILLISECOND, mss);
+        long utc = calendar.getTimeInMillis();
+
+        jsonNode = jsonNode.put("unixtime", utc);
+
+        return jsonNode;
     }
 
 
-    private static Client establishESConnection() throws UnknownHostException {
-        Client client = null;
-        try {
-            client = TransportClient.builder().build()
-                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("hack-es"), 9300));
-            LOGGER.info("Connection to ElasticSearch established !");
-        }
-        catch (Exception e){
 
-            LOGGER.error("ES connection error", e);
-        }
-
-        return client;
+    private MongoClient establishESConnection() throws UnknownHostException {
+        MongoClient mongo = new MongoClient("hack-es", 27017);
+        return mongo;
     }
 
 }
