2d72268f699be3e1c3775fd42e6b39f66e821b23
Cristian Schuszter <cristian.schuszter@atigeo.com>
2016-05-22 11:23:08 +0300

    Added Realtime data capabilities

--- ui/app/ApiCarDataService.js
+++ ui/app/ApiCarDataService.js
@@ -11,7 +11,9 @@
             var baseUrl = 'http://52.53.195.124:8080';
 
             var service = {
-                GetPoints: getPoints
+                GetPoints: getPoints,
+                InitRealtimePoints: initRealtimePoints,
+                PollRealtime: pollRealtime
             };
 
 
@@ -21,6 +23,23 @@
                 return $http({
                   method: 'GET',
                   url: baseUrl + '/points',
+                });
+            }
+
+            function initRealtimePoints(){
+                return $http({
+                    method: 'GET',
+                    params: {'realtime': true},
+                    url: baseUrl + '/points'
+                });
+            }
+
+
+            function pollRealtime(){
+                return $http({
+                    method: 'GET',
+                    params: {'start_ts' : Date.now()},
+                    url: baseUrl + '/points'
                 });
             }
 
