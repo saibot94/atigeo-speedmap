29b1a9bfbee522a0d4468e66443120f5a7579768
Ubuntu <ubuntu@ip-172-31-1-21.us-west-1.compute.internal>
2016-05-22 06:39:14 +0000

    Version 0.1 -- wokring

--- api/api.py
+++ api/api.py
@@ -4,10 +4,13 @@ from flask import Flask
 from flask import jsonify
 from flask import make_response
 from flask import request
+from flask.ext.cors import CORS
+
 
 from domain.mongo import get_points_with_weight
 
 app = Flask(__name__)
+CORS(app)
 
 
 app_conf = ConfigParser.ConfigParser()
@@ -17,24 +20,16 @@ app_conf.read("atism.ini")
 LEGAL_SPEED_LIMIT = app_conf.get("Legal", "speed")
 
 
-@app.after_request
-def after_request(response):
-  return response
-
-
 @app.route('/points', methods=["GET"])
 def get_drive_points():
     start_ts = request.args.get('start_ts')
     end_ts = request.args.get('end_ts')
 
-    points = get_points_with_weight(count=500)
+    points = get_points_with_weight(count=10000)
 
     response = make_response(jsonify({"points": points}))
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
 
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
--- api/domain/mongo.py
+++ api/domain/mongo.py
@@ -6,17 +6,32 @@ DB = "hacktm"
 client = MongoClient(host=MONGO_HOST)
 db = client.get_database(DB)
 
-DISPLAY_SPEED = 10  # TODO: get from config
+DISPLAY_SPEED = 16  # TODO: get from config
 
 
 def get_points(count=None):
     points_cursor = db.drives.find({"speed": {"$gte": DISPLAY_SPEED}},
-                                   {'latitude': 1, 'longitude': 1, 'speedkmh': 1, '_id': 0})
+                                   {'latitude': 1, 'longitude': 1, 'speedkmh': 1, 'unixtime':1, '_id': 0})
     return points_cursor if not count else points_cursor.limit(count)
 
 
 def new_point(point):
-    return {'latitude': point['latitude'], 'longitude': point['longitude'], 'weight': point['speedkmh'] / 10}
+    speed = point['speedkmh']
+    if 40 < speed < 50:
+        display = speed / 1.2456
+    elif  50 < speed < 60:
+        display = speed 
+    else: 
+       display = speed * 1.22
+
+    new_point = {}
+    for k, v in point.iteritems():
+        if k == 'speed': 
+            new_point['weight'] = display
+        else:
+            new_point[k] = v
+
+    return new_point
 
 
 def get_points_with_weight(count=None):
